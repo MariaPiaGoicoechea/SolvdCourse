2e27d2ceb49713e81029f3b8db73372b5ff11952
Morillaz <gomezmorillaz@gmail.com>
2021-04-04 16:13:31 -0300

    Minor modification to Hello.java

--- src/main/java/com/solvd/hello/hello.java
+++ src/main/java/com/solvd/hello/hello.java
@@ -7,11 +7,9 @@ public class hello {
     private final static Logger logger = LogManager.getLogger(hello.class);
     public static String main(String[] args) {
         if(args != null){
-            logger.info("Greeting returned");
-            return "Hello " + args[0] + "!";
+            logger.info("Hello " + args[0] + "!");
         } else{
-            logger.info("Greeting returned");
-            return "Hello git!";
+            logger.info("Hello git!");
         }
     }
 }
