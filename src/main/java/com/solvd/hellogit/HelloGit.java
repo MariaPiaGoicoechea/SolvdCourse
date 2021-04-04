68a55af1644cfaf5d7fa328f585b97bc14bb58ad
tincholorenzo <tincholorenz@gmail.com>
2021-04-04 19:42:08 -0300

    First commit - Martin

--- src/main/java/com/solvd/hellogit/HelloGit.java
+++ src/main/java/com/solvd/hellogit/HelloGit.java
@@ -1,7 +1,10 @@
 package com.solvd.hellogit;
 
+import org.apache.commons.lang3.stream.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.solvd.hellogit.utils.LeapYearCalculator;
 
 import java.util.Arrays;
 import java.util.regex.Matcher;
@@ -23,19 +26,31 @@ public class HelloGit {
 			if(m.matches()){
 				log.info("Computing Fibonacci");
 				f.assignFiboNums(Integer.parseInt(args[0]));
-				log.info(Arrays.toString(f.calculateFiboNums()));
+				int [] fiboNums = f.calculateFiboNums();
+				log.info(Arrays.toString(fiboNums));
 
 				log.info("Computing Collatz");
 				log.info(Collatz.calculateSteps(Integer.parseInt(args[0])));
 
+				LeapYearCalculator leapYearCalculator = new LeapYearCalculator();
+				Arrays.stream(fiboNums)
+					.filter(y -> leapYearCalculator.isLeapYear(y))
+					.forEach(ly-> log.info(ly));
 			}else genericInput(f);
 		}else genericInput(f);
+		
 	}
 
 	public static void genericInput(Fibonacci f){
 		log.info("Generic Fibonacci and Collatz with 20");
-		f.assignFiboNums(20);
-		log.info(Arrays.toString(f.calculateFiboNums()));
+		f.assignFiboNums(40);
+		int [] fiboNums = f.calculateFiboNums();
+		log.info(Arrays.toString(fiboNums));
 		log.info(Collatz.calculateSteps(20));
+		
+		LeapYearCalculator leapYearCalculator = new LeapYearCalculator();
+		Arrays.stream(fiboNums)
+			.filter(y -> leapYearCalculator.isLeapYear(y))
+			.forEach(ly-> log.info(ly));
 	}
 }
